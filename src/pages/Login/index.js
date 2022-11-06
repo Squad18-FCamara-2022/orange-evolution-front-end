a0a3be53904584777ae87b87eea960180151d4af
Paula Dias <paulafmadias@gmail.com>
2022-11-06 08:59:54 -0300

    config requisições api login e cadastro

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,16 +1,25 @@
-import './styles.css';
-import { Link } from 'react-router-dom';
-import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link, useNavigate } from 'react-router-dom';
 import * as yup from 'yup';
+import './styles.css';
+import { setLocalItem } from '../../utils/localStorage';
+import api from '../../services/api';
+import { warning } from '@remix-run/router';
 
 const errorMessages = {
-  required: 'Campo obrigatório',
+  email: 'Digite um e-mail válido',
+  password: 'A senha deve ter entre 4 e 10 caracteres',
 };
 
 const validationSchema = yup.object().shape({
-  email: yup.string().email().required(errorMessages.required),
-  password: yup.string().min(4).max(10).required(),
+  email: yup.string().email(errorMessages.email).required(errorMessages.email),
+  password: yup
+    .string()
+    .min(4, errorMessages.password)
+    .max(10, errorMessages.password)
+    .required(errorMessages.password),
 });
 
 function Login() {
@@ -22,8 +31,35 @@ function Login() {
     resolver: yupResolver(validationSchema),
   });
 
-  const onSubmit = (data) => {
-    console.log(data);
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+  });
+  const handleFormChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const navigate = useNavigate();
+  const navigateHome = () => {
+    navigate('/home');
+  };
+
+  const [warning, setWarning] = useState();
+  const onSubmit = async (data, e) => {
+    e.preventDefault();
+    console.log(form);
+
+    try {
+      const response = await api.post('/signin', {
+        ...form,
+      });
+      setLocalItem('token', response.token);
+      setLocalItem('userId', response.user.id);
+      setLocalItem('userName', response.user.name);
+      navigateHome();
+    } catch (error) {
+      setWarning(error.response.data);
+    }
   };
 
   return (
@@ -36,6 +72,8 @@ function Login() {
             name="email"
             placeholder="Digite seu e-mail"
             {...register('email')}
+            value={form.email}
+            onChange={(e) => handleFormChange(e)}
           />
           <p>{errors.email?.message}</p>
           <input
@@ -43,8 +81,11 @@ function Login() {
             name="password"
             placeholder="Digite sua senha"
             {...register('password')}
+            value={form.password}
+            onChange={(e) => handleFormChange(e)}
           />
           <p>{errors.password?.message}</p>
+          <p>{warning}</p>
           <button type="submit">Entrar</button>
         </form>
       </div>
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,28 +1,31 @@
-import './styles.css';
-import { Link } from 'react-router-dom';
-import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link, useNavigate } from 'react-router-dom';
 import * as yup from 'yup';
+import api from '../../services/api';
+import './styles.css';
 
 const errorMessages = {
-  required: 'Campo obrigatório',
+  name: 'Nome obrigatório',
   email: 'Digite um e-mail válido',
   password: 'A senha deve ter entre 4 e 10 caracteres',
   confirmPassword: 'Senhas não conferem',
 };
 
 const validationSchema = yup.object().shape({
-  name: yup.string().trim().required(errorMessages.required),
-  email: yup
+  name: yup
     .string()
-    .email(errorMessages.email)
-    .required(errorMessages.required),
+    .trim()
+    .min(4, errorMessages.name)
+    .required(errorMessages.name),
+  email: yup.string().email(errorMessages.email).required(errorMessages.email),
   password: yup
     .string()
     .trim()
     .min(4, errorMessages.password)
     .max(10, errorMessages.password)
-    .required(),
+    .required(errorMessages.password),
   confirmPassword: yup
     .string()
     .oneOf([yup.ref('password'), null], errorMessages.confirmPassword),
@@ -37,8 +40,39 @@ function Register() {
     resolver: yupResolver(validationSchema),
   });
 
-  const onSubmit = (data) => {
-    console.log(data);
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+  const handleFormChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const navigate = useNavigate();
+  const navigateLogin = () => {
+    navigate('/');
+  };
+
+  const [warning, setWarning] = useState();
+  const onSubmit = async (data, e) => {
+    e.preventDefault();
+    console.log(form);
+
+    try {
+      await api.post('/signup', {
+        ...form,
+      });
+      setWarning(
+        'Usuário cadastrado com sucesso! Você será redirecionado para a página de login.'
+      );
+      setTimeout(() => {
+        navigateLogin();
+      }, 2000);
+    } catch (error) {
+      setWarning(error.response.data);
+    }
   };
 
   return (
@@ -51,13 +85,17 @@ function Register() {
             name="name"
             placeholder="Digite seu nome"
             {...register('name')}
+            value={form.nome}
+            onChange={(e) => handleFormChange(e)}
           />
-          <p>{errors.email?.message}</p>
+          <p>{errors.name?.message}</p>
           <input
             type="text"
             name="email"
             placeholder="Digite seu e-mail"
             {...register('email')}
+            value={form.email}
+            onChange={(e) => handleFormChange(e)}
           />
           <p>{errors.email?.message}</p>
           <input
@@ -65,6 +103,8 @@ function Register() {
             name="password"
             placeholder="Digite sua senha"
             {...register('password')}
+            value={form.password}
+            onChange={(e) => handleFormChange(e)}
           />
           <p>{errors.password?.message}</p>
           <input
@@ -72,9 +112,12 @@ function Register() {
             name="confirmPassword"
             placeholder="Repita sua senha"
             {...register('confirmPassword')}
+            value={form.confirmPassword}
+            onChange={(e) => handleFormChange(e)}
           />
           <p>{errors.confirmPassword?.message}</p>
-          <button type="submit">Entrar</button>
+          <p>{warning}</p>
+          <button type="submit">Enviar</button>
         </form>
       </div>
       <p className="login-route">
