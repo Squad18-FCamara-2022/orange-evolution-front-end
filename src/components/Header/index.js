58b0accfecc559ad70699743291852c2da3c1d0e
Paula Dias <paulafmadias@gmail.com>
2022-11-08 20:00:07 -0300

    aumento timeout axios

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,23 +1,10 @@
 import './styles.css';
 
-function Header() {
+function Header(page) {
   return (
     <div className="container-header">
       <h1>Header</h1>
-      <nav>
-        <ul>
-          <li>Dev FullStack</li>
-          <li>QA</li>
-          <li>UX/UI</li>
-        </ul>
-        <a
-          href="https://discord.gg/B8KYUqVWM4"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Discord
-        </a>
-      </nav>
+      <nav></nav>
     </div>
   );
 }
