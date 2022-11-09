e271c89d0b5fbf03f8f668f597798277bc8f23d8
Paula Dias <paulafmadias@gmail.com>
2022-11-09 15:26:53 -0300

    config para deploy netlify

--- src/pages/UserClasses/index.js
+++ src/pages/UserClasses/index.js
@@ -1,66 +1,66 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import Header from '../../components/Header';
-import api from '../../services/api';
+// import api from '../../services/api';
 import './styles.css';
-import { getLocalItem } from '../../utils/localStorage';
-import { useLocation } from 'react-router-dom';
+// import { getLocalItem } from '../../utils/localStorage';
+// import { useLocation } from 'react-router-dom';
 
 function UserClasses() {
   // get da api para listar todas as aulas de uma trilha e as feitas pelo usuário
   // post para api para registrar aulas feitas por aquele usuário (fazer local e mandar pra api) - ver sobre a tabela que virá
   // delete para api para excluir aulas feitas por aquele usuário (fazer local e mandar pra api) - ver sobre a tabela que virá
 
-  const [classes, setClasses] = useState();
-  const [localClasses, setLocalClasses] = useState();
-  const { token, userId } = getLocalItem('token', 'userId');
-  const location = useLocation();
-  const { track } = location.state;
+  // const [classes, setClasses] = useState();
+  // const [localClasses, setLocalClasses] = useState();
+  // const { token, userId } = getLocalItem('token', 'userId');
+  // const location = useLocation();
+  // const { track } = location.state;
 
-  const getClassesUser = async ({ token, userId, trackName }) => {
-    try {
-      const { data } = await api.get(`/class/${userId}/?track=${trackName}`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      setClasses(data);
-      setLocalClasses(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  // const getClassesUser = async ({ token, userId, trackName }) => {
+  //   try {
+  //     const { data } = await api.get(`/class/${userId}/?track=${trackName}`, {
+  //       headers: {
+  //         Authorization: `Bearer ${token}`,
+  //       },
+  //     });
+  //     setClasses(data);
+  //     setLocalClasses(data);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
-  const addDoneClass = async ({ userId, classId }) => {
-    try {
-      const { data } = await api.post(`/class/${userId}/?class=${classId}`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      //fazer a atualização das aulas locais (decidir com lucas sobre trazer do back ou no front)
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  // const addDoneClass = async ({ userId, classId }) => {
+  //   try {
+  //     const { data } = await api.post(`/class/${userId}/?class=${classId}`, {
+  //       headers: {
+  //         Authorization: `Bearer ${token}`,
+  //       },
+  //     });
+  //     //fazer a atualização das aulas locais (decidir com lucas sobre trazer do back ou no front)
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
-  const deleteDoneClass = async ({ userId, classId }) => {
-    try {
-      const { data } = await api.delete(`/class/${userId}/?class=${classId}`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      //fazer a atualização das aulas locais (decidir com lucas sobre trazer do back ou no front)
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  // const deleteDoneClass = async ({ userId, classId }) => {
+  //   try {
+  //     const { data } = await api.delete(`/class/${userId}/?class=${classId}`, {
+  //       headers: {
+  //         Authorization: `Bearer ${token}`,
+  //       },
+  //     });
+  //     //fazer a atualização das aulas locais (decidir com lucas sobre trazer do back ou no front)
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
   //passar add e delete como prop para os componentes linhas da tabela de aulas
 
-  useEffect(() => {
-    getClassesUser(token, userId, track);
-  });
+  // useEffect(() => {
+  //   getClassesUser(token, userId, track);
+  // });
 
   return (
     <div className="user-classes-container">
